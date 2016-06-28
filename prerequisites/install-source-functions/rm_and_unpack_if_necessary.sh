80ec7536ee0720e71c90642b4b8b98e4164b8bba
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-06-28 10:04:26 -0700

    Removes package and unpacks if necessary.
    The removal of the package is required to make sure no prior
    configurations had been done.

---
+++ prerequisites/install-source-functions/rm_and_unpack_if_necessary.sh
@@ -0,0 +1,24 @@
+# Unpack if the unpacked tar ball is not in the present working directory
+# shellcheck disable=SC2154
+rm_and_unpack_if_necessary()
+{
+  if [[ "${fetch}" == "svn" || "${fetch}" == "git" ]]; then
+    package_source_directory="${version_to_build}"
+  else
+    info "Unpacking ${url_tail}."
+    info "pushd ${download_path}"
+    pushd "${download_path}"
+    subpackage_list="aterm-2.5 sdf2-bundle-2.4 strategoxt-0.17"
+    for subpackage in "${subpackage_list[@]}"
+    do
+	info "rm -rf ${subpackage}"
+	rm -rf ${subpackage}
+    done 
+    info "Unpack command: tar zxf ${url_tail}"
+    tar zxf "${url_tail}"
+    info "popd"
+    popd
+    # shellcheck disable=SC2034
+    package_source_directory="${package_name}-${version_to_build}"
+  fi
+}
